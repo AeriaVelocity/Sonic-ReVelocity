8b925fbbb537bd31ad9559622177eb33cdd565fd
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-01-31 19:27:01 +0000

    Update Sonic's movement code
    Sonic is now marked as dead when he falls down a pit, which will affect the bottom HUD.
    Additionally, made some improvements to the movement sound.

--- SonicMovement.gd
+++ SonicMovement.gd
@@ -5,9 +5,12 @@ var acceleration = 400.0
 var deceleration = 200.0
 var jump_speed = -500.0
 
+var dead = false
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var start_position = position
+var was_on_floor: bool
 
 @onready var movement_sound = $MovementSound
 @onready var jump_sound = $JumpSound
@@ -23,7 +26,9 @@ func update_speed_rank():
 	var rank_object: Label = get_node("/root/Node2D/CanvasLayer/BottomHUD/SpeedRank")
 	var rank: String
 	var speed = sqrt(velocity.x ** 2 + velocity.y ** 2)
-	if speed >= 1500:
+	if dead:
+		rank = "You are ded, not big soup rice!"
+	elif speed >= 1500:
 		rank = "SPEED FASTEST!!!"
 	elif speed >= 1000:
 		rank = "Now That's What I Call Speed™!"
@@ -33,12 +38,26 @@ func update_speed_rank():
 		rank = "Come on! Speed up already!"
 	else:
 		rank = "You're barely even moving!"
-	speedometer.set_text("Speedometer: %d fasts per speed" % speed)
-	rank_object.set_text("Speed Rank: " + rank)
+	if not dead:
+		speedometer.set_text("Speedometer: %d fasts per speed" % speed)
+		rank_object.set_text("Speed Rank: " + rank)
+	else:
+		speedometer.set_text("DEADometer: Yes DEATHS per DEAD")
+		rank_object.set_text("DEAD Rank: " + rank)
+	
+func handle_movement_sound():
+	movement_sound.playing = abs(velocity.x) > 0 and is_on_floor()
+	
+func _ready():
+	was_on_floor = is_on_floor()
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("Jump") and is_on_wall():
+		velocity.y = jump_speed
+		jump_sound.play()
 
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_speed
@@ -56,12 +75,20 @@ func _physics_process(delta):
 			current_velocity.x = min(0, current_velocity.x + deceleration * delta)
 
 	velocity.x = current_velocity.x
-
+	
 	if Input.is_action_just_pressed("MoveLeft") or Input.is_action_just_pressed("MoveRight"):
-		movement_sound.playing = true
+		handle_movement_sound()
+		
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		movement_sound.stop()
 
-	if Input.is_action_just_released("MoveLeft") or Input.is_action_just_released("MoveRight"):
-		movement_sound.playing = false
+	if velocity.x == 0:
+		handle_movement_sound()
+		
+	if is_on_floor() and not was_on_floor:
+		handle_movement_sound()
+
+	was_on_floor = is_on_floor()
 
 	move_and_slide()
 	update_speed_rank()
@@ -69,7 +96,9 @@ func _physics_process(delta):
 	current_velocity = velocity
 
 func _on_death_area_body_entered(body):
+	dead = true
 	death_sound.play()
 	current_velocity.x = 0
 	await(death_sound.finished)
 	set_position(start_position)
+	dead = false
