ea0ca764fc430f4f845303930dbd68f35c4154f8
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-02-01 12:03:30 +0000

    Removed comments in IntroVideo

--- IntroVideo.gd
+++ IntroVideo.gd
@@ -1,13 +1,10 @@
 extends VideoStreamPlayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var window_size = get_viewport_rect().size
 	size = window_size
 	await(finished)
 	get_tree().change_scene_to_file("res://green-hill-simulator.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
