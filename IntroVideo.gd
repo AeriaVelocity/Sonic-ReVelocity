9a00f23d6738d9da07b000c63727e637f5aa01e8
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-02-01 13:22:59 +0000

    Updated intro video and added a skip function
    The skip prompt will now also ask for a specific button depending on the first controller that's plugged in, or if none are plugged in.

--- IntroVideo.gd
+++ IntroVideo.gd
@@ -7,4 +7,20 @@ func _ready():
 	get_tree().change_scene_to_file("res://green-hill-simulator.tscn")
 
 func _process(delta):
-	pass
+	var skip_hint = $CanvasLayer/SkipHint
+	var hint: String
+	match Input.get_joy_name(0):
+		"XInput Gamepad":
+			hint = "Press [color=green]A[/color] to skip"
+		"PS4 Controller", "PS5 Controller":
+			hint = "Press [img=24]playstation-cross.png[/img] to skip"
+		"Nintendo Switch Pro Controller":
+			hint = "Press B to skip"
+		"":
+			hint = "Press SPACE to skip"
+		_:
+			hint = "Press JUMP to skip"
+	skip_hint.set_text("[center][font_size=24]" + hint + "[/font_size][/center]")
+	if Input.is_action_just_pressed("Jump"):
+		stop()
+		get_tree().change_scene_to_file("res://green-hill-simulator.tscn")
