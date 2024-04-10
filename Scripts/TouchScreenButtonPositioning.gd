b06380b63a40811c85b6cc48f7134dee182c9fea
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-04-10 18:05:25 +0100

    Updated HUD stuff

--- Scripts/TouchScreenButtonPositioning.gd
+++ Scripts/TouchScreenButtonPositioning.gd
@@ -7,5 +7,5 @@ func _process(_delta):
 	var left_position = Vector2(20, buttons_height)
 	$Left.position = left_position
 
-	var right_position = Vector2(viewport_size.x - 150, buttons_height)
+	var right_position = Vector2(viewport_size.x - 60, buttons_height)
 	$Right.position = right_position
