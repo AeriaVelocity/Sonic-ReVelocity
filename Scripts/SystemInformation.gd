e6e679aa85bdb2eb540c78d3179e1c311c56a686
Arsalan "Aeria" Kazmi <sonicspeed848@gmail.com>
2024-06-23 16:41:49 +0100

    Updated the System Information page

--- Scripts/SystemInformation.gd
+++ Scripts/SystemInformation.gd
@@ -9,32 +9,79 @@ func handle_exit():
 			get_tree().change_scene_to_file("res://main-menu.tscn")
 	elif Input.is_action_just_pressed("Spin"):
 		get_tree().change_scene_to_file("res://main_menu.tscn")
+		
+func get_inputs() -> Dictionary:
+	var inputs: Dictionary = {}
+	var action_count = InputMap.get_actions()
+	for i in action_count:
+		var action = InputMap.action_get_events(i)
+		if i.begins_with("ui_") or action.size() <= 0:
+			continue
+		inputs[i] = str(action)
+	return inputs
+
+func format_memory(mem: int) -> String:
+	var megabytes = mem / 1024.0 / 1024.0
+	return "%d MB" % megabytes
 
 func update_info():
 	var os_name: String = OS.get_distribution_name() + " " + OS.get_version()
-	var is_touchscreen: bool = DisplayServer.is_touchscreen_available()
+	var engine_ver: String = Engine.get_version_info().string
+	var memory: String = format_memory(OS.get_memory_info().free)
+	var total_memory: String = format_memory(OS.get_memory_info().available)
+	var is_touchscreen: String = "Yes" if DisplayServer.is_touchscreen_available() else "No"
+	var inputs: Dictionary = get_inputs()
 	var controller_count = Input.get_connected_joypads().size()
 	var controller_names = []
 	for i in controller_count:
+		var to_append = Input.get_joy_name(i) + " (GUID " + Input.get_joy_guid(i) + ")"
 		if !Input.is_joy_known(i):
-			continue
-		controller_names.append(Input.get_joy_name(i) + " (GUID " + Input.get_joy_guid(i) + ")")
+			to_append = "ILLEGAL: " + to_append + " (This device is not a valid controller.)"
+		controller_names.append(to_append)
 
 	sysinfo_text = """=== Sonic Re;Velocity System Information ===
 
-Operating system: %s
-Touchscreen: %s
-Connected controllers:
-""" % [os_name, is_touchscreen]
+== To Non-Developers ==
+This system information is intended for developers of Sonic Re;Velocity. You aren't expected to understand this.
+If you're not a developer, you probably got here by mistake.
+If you ARE a developer, you still probably got here by mistake.
+If you're an Easter egg hunter, congratulations! You win! You got the Hidden System Information Page Ending! I hope your friends find this concerning.
+If you're a hacker looking for ways to rip the game apart, you won't find it here. (But maybe you can find it somewhere...)
 
-	for controller_name in controller_names:
-		sysinfo_text += "- " + controller_name + "\n"
+== Basic information ==
+Operating system: %s
+Memory (RAM): %s/%s
+Godot Engine version: %s
+Touchscreen detected: %s
+
+""" % [os_name, memory, total_memory, engine_ver, is_touchscreen]
+
+	sysinfo_text += "== Controller information ==\n"
+	sysinfo_text += "Connected controllers: \n"
+
+	if controller_names.size() > 0:
+		var index = 0
+		for controller_name in controller_names:
+			sysinfo_text += str(index) + ". " + controller_name + "\n"
+			index += 1
+	else:
+		sysinfo_text += "- No \"real\" controllers detected. \"Real\" controllers are controllers recognised by Godot's `Input.is_joy_known()` function.\n"
+
+	sysinfo_text += "Input mappings: \n"
+
+	for input in inputs:
+		sysinfo_text += "- " + input + ": " + inputs[input] + "\n"
 
 	sysinfo_label.text = sysinfo_text
 
-func _process(_delta):
+func _ready():
 	update_info()
+
+func _process(_delta):
 	handle_exit()
 
 func _on_copy_button_button_down():
 	DisplayServer.clipboard_set(sysinfo_text)
+
+func _on_update_button_button_down():
+	update_info()
