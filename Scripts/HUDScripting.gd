826e7383332385bd58ebc1e8ca2092cffb9e07fc
Arsalan "Aeria" Kazmi <sonicspeed848@gmail.com>
2024-06-27 10:47:47 +0100

    Speedometer now only counts horizontal velocity

--- Scripts/HUDScripting.gd
+++ Scripts/HUDScripting.gd
@@ -6,7 +6,7 @@ func _ready():
     pass # Replace with function body.
 
 func update_speed(velocity: Vector2):
-    speed = sqrt(velocity.x ** 2 + velocity.y ** 2)
+    speed = abs(velocity.x)
 
 func get_speed() -> float:
     return speed
