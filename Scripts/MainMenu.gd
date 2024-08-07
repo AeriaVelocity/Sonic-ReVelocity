1ba1d5ac05ae25dce3b1262400f8f890e973f2b2
Arsalan "Aeria" Kazmi <sonicspeed848@gmail.com>
2024-08-07 18:36:54 +0100

    Updated the title screen and main menu theme
    The title screen now uses a darker and more subdued colour scheme. The menu screen has received a bit of a facelift. A new Options item has been added, which doesn't do anything.

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -5,13 +5,18 @@ var selected_option_index: int
 var highlight: Sprite2D
 var highlight_anim: AnimationPlayer
 var menu_options: VBoxContainer
+var tooltip: Label
+var unimplemented_popup: Control
 
 func _ready():
     highlight = $Highlight
+    highlight.global_position.x = -30
     highlight_anim = $Highlight/AnimationPlayer
     menu_options = $MenuOptions
+    tooltip = $Tooltip
 
     selected_option_index = 0
+    update_highlight_position()
 
 func update_highlight_position():
     highlight_anim.stop()
@@ -27,6 +32,8 @@ func update_highlight_position():
             option.modulate = Color(1, 1, 1)
 
 func handle_input():
+    if unimplemented_popup and unimplemented_popup.visible:
+        return
     var sysinfo_prereq = Input.is_action_pressed("SysinfoPrereq1") and Input.is_action_pressed("SysinfoPrereq1")
     if Input.is_action_just_pressed("MenuUp"):
         selected_option_index = (selected_option_index - 1 + menu_options.get_child_count()) % menu_options.get_child_count()
@@ -51,13 +58,29 @@ func handle_input():
     elif sysinfo_prereq and Input.is_action_just_pressed("Sysinfo"):
         get_tree().change_scene_to_file("res://system_information.tscn")
 
+func handle_tooltip():
+    if selected_option_index == 0:
+        tooltip.text = "Jump right into Sonic Re;Velocity and play the test level."
+    elif selected_option_index == 1:
+        tooltip.text = "Configure options and settings, including controller bindings."
+    elif selected_option_index == 2:
+        tooltip.text = "Quit Sonic Re;Velocity and return to %s." % OS.get_name().capitalize()
+    else:
+        tooltip.text = ""
+
 func activate_selected_option():
     match selected_option_index:
         0:
             LevelStats.rings = 0
             get_tree().change_scene_to_file("res://test-level.tscn")
         1:
+            unimplemented_popup = preload("res://popup.tscn").instantiate()
+            add_child(unimplemented_popup)
+            unimplemented_popup.title_text = "Unimplemented"
+            unimplemented_popup.message_text = "The options menu is not yet implemented.\nPlease check back in a future update."
+        2:
             get_tree().quit()
 
 func _process(_delta):
     handle_input()
+    handle_tooltip()
